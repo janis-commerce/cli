533198b337d5a864e3815e153d78a39ff011a493
Mauricio Oruezabal <mauricio.oruezabal@gmail.com>
2022-02-15 18:37:01 -0300

    [JCN-349-create-lambda-functions-with-janis-cli] - add Core-Functions tag in schema template

--- tests/templates/function-schema.js
+++ tests/templates/function-schema.js
@@ -7,9 +7,10 @@ const template = require('../../lib/templates/function-schema');
 describe('Templates', () => {
 	describe('Function with payload schema', () => {
 
-		it('Should return the function schema ', () => {
+		it('Should return the function schema (with Client) ', () => {
 			const result = template({
 				functionName: 'publish-products',
+				hasClient: true,
 				hasPayload: true
 			});
 
@@ -54,13 +55,63 @@ describe('Templates', () => {
 				}
 			});
 		});
+
+		it('Should return the function schema (without Client) ', () => {
+			const result = template({
+				functionName: 'publish-products',
+				hasClient: false,
+				hasPayload: true
+			});
+
+			assert.deepStrictEqual(result, {
+				paths: {
+					'/publish-products': {
+						post: {
+							'x-janis-namespace': undefined,
+							'x-janis-method': undefined,
+							'x-janis-permissions': [],
+							operationId: 'publish-products',
+							requestBody: {
+								description: 'publish-products',
+								required: true,
+								content: {
+									'application/json': {}
+								}
+							},
+							summary: 'publish-products',
+							tags: [
+								'Core-Functions'
+							],
+							responses: {
+								200: {
+									$ref: '#/components/responses/GenericSuccess'
+								},
+								400: {
+									$ref: '#/components/responses/BadRequest'
+								},
+								401: {
+									$ref: '#/components/responses/Unauthorized'
+								},
+								403: {
+									$ref: '#/components/responses/Forbidden'
+								},
+								500: {
+									$ref: '#/components/responses/InternalError'
+								}
+							}
+						}
+					}
+				}
+			});
+		});
 	});
 
 	describe('Function without payload schema', () => {
 
-		it('Should return the function schema ', () => {
+		it('Should return the function schema (with Client)', () => {
 			const result = template({
 				functionName: 'publish-products',
+				hasClient: true,
 				hasPayload: false
 			});
 
@@ -75,6 +126,48 @@ describe('Templates', () => {
 							summary: 'publish-products',
 							tags: [
 								'Functions'
+							],
+							responses: {
+								200: {
+									$ref: '#/components/responses/GenericSuccess'
+								},
+								400: {
+									$ref: '#/components/responses/BadRequest'
+								},
+								401: {
+									$ref: '#/components/responses/Unauthorized'
+								},
+								403: {
+									$ref: '#/components/responses/Forbidden'
+								},
+								500: {
+									$ref: '#/components/responses/InternalError'
+								}
+							}
+						}
+					}
+				}
+			});
+		});
+
+		it('Should return the function schema (without Client)', () => {
+			const result = template({
+				functionName: 'publish-products',
+				hasClient: false,
+				hasPayload: false
+			});
+
+			assert.deepStrictEqual(result, {
+				paths: {
+					'/publish-products': {
+						post: {
+							'x-janis-namespace': undefined,
+							'x-janis-method': undefined,
+							'x-janis-permissions': [],
+							operationId: 'publish-products',
+							summary: 'publish-products',
+							tags: [
+								'Core-Functions'
 							],
 							responses: {
 								200: {
